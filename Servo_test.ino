1b17259cc283e341f47aa13f9c087a256d297958
Unknown <hannes.hedlund94@gmail.com>
2017-09-07 09:43:36 +0200

    Working arduino code for servo and phototransistor.

--- Servo_test.ino
+++ Servo_test.ino
@@ -15,48 +15,42 @@
 #define FALSE 0
 #define TRUE 1
 
-/* Static variables */
 Servo myservo;
-int servoPosition;
+int servoPosition = 0;
 int light;
 bool closeDone;
 bool openDone;
 
 void setup() {
-    Serial.begin(9600);
-    myservo.attach(9);
+     Serial.begin(9600);
+     myservo.attach(9);
+     pinMode(7,INPUT);
 }
 
 void loop() {
-    servoPosition = 0;
-    light = analogRead(A0);
-    Serial.println(light);
+     light = analogRead(A0);
+     Serial.println(light);
 
-
-  /* Close blinds when light is high, and blinds are not already closed */
-  if(light >= 150 && closeDone == FALSE){
-        for (servoPosition = 0; servoPosition <= 180; servoPosition += 1) { // goes from 0 degrees to 180 degrees
-            // in steps of 1 degree
-            myservo.write(servoPosition);              // tell servo to go to servoPositionition in variable 'servoPosition'
-            delay(15);                       // waits 15ms for the servo to reach the servoPositionition
-            openDone = FALSE;
-            closeDone = FALSE;
-            if(servoPosition == 180){
-                closeDone = TRUE;
-            }
-        }
-    }
-
-    /* Open blinds when light is low, and blinds are not already opened */
-    if(light < 150 && openDone == FALSE) {
-        for (servoPosition = 180; servoPosition >= 0; servoPosition -= 1) {    // goes from 180 degrees to 0 degrees
-            myservo.write(servoPosition);                  // tell servo to go to servoPositionition in variable 'servoPosition'
-            delay(15);                           // waits 15ms for the servo to reach the servoPositionition
-            openDone = FALSE;
-            closeDone = FALSE;
-            if(servoPosition == 0){
-                openDone = TRUE;
-            }
-        }
-    }
-}
+/* Close blinds when light is high, and blinds are not already closed */
+    if(light >= 150 && closeDone == FALSE){
+             myservo.write(pos);
+             delay(15);
+             pos++;
+             openDone = FALSE;
+             closeDone = FALSE;
+             if(pos == 180){
+               closeDone = TRUE;
+             }
+         }
+         /* Open blinds when light is low, and blinds are not already opened */
+         if(light < 150 && openDone == FALSE) {
+             myservo.write(pos);
+             delay(15);
+             pos--;
+             openDone = FALSE;
+             closeDone = FALSE;
+             if(pos == 0){
+                 openDone = TRUE;
+           }
+         }
+       }
