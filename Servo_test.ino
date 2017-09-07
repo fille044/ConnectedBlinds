b9913cea1efd3f94a5fc3cdb4c763d45d6ba2884
Filip Lindström <fille044@gmail.com>
2017-09-07 09:05:13 +0200

    Only esthetic changes
    Added a file-header.
    Removed input on D7.
    Indented to standard 4 spaces per tab.

--- Servo_test.ino
+++ Servo_test.ino
@@ -1,47 +1,62 @@
+/* ------------------------------------------------------------------------
+   First test of controlling the blinds using an Arduino Uno and the library
+   Servo.h.
+   The servo rotates from 0-180 degrees based on the light on a phototransistor.
+
+   openDone and closeDone are flags to detect when blinds have come to end-point.
+
+   Servo           on D9
+   Phototransistor on A0
+   ------------------------------------------------------------------------ */
+
+
+
 #include <Servo.h>
 #define FALSE 0
 #define TRUE 1
 
+/* Static variables */
 Servo myservo;
-int pos;
+int servoPosition;
 int light;
-bool closeDone, openDone;
+bool closeDone;
+bool openDone;
 
 void setup() {
-  Serial.begin(9600);
-  myservo.attach(9);
-  pinMode(7,INPUT);  
+    Serial.begin(9600);
+    myservo.attach(9);
 }
 
 void loop() {
-  pos = 0;
-  light = analogRead(A0);
-  Serial.println(light);
+    servoPosition = 0;
+    light = analogRead(A0);
+    Serial.println(light);
 
-   
-  //Close blinds
+
+  /* Close blinds when light is high, and blinds are not already closed */
   if(light >= 150 && closeDone == FALSE){
-      for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-      // in steps of 1 degree
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-      openDone = FALSE;
-      closeDone = FALSE;
-      if(pos == 180){
-        closeDone = TRUE;
-      }
+        for (servoPosition = 0; servoPosition <= 180; servoPosition += 1) { // goes from 0 degrees to 180 degrees
+            // in steps of 1 degree
+            myservo.write(servoPosition);              // tell servo to go to servoPositionition in variable 'servoPosition'
+            delay(15);                       // waits 15ms for the servo to reach the servoPositionition
+            openDone = FALSE;
+            closeDone = FALSE;
+            if(servoPosition == 180){
+                closeDone = TRUE;
+            }
+        }
     }
-  }
-  //Open blinds
-  if(light < 150 && openDone == FALSE) {
-      for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(15);                       // waits 15ms for the servo to reach the position
-      openDone = FALSE;
-      closeDone = FALSE;
-      if(pos == 0){
-        openDone = TRUE;
-      }
+
+    /* Open blinds when light is low, and blinds are not already opened */
+    if(light < 150 && openDone == FALSE) {
+        for (servoPosition = 180; servoPosition >= 0; servoPosition -= 1) {    // goes from 180 degrees to 0 degrees
+            myservo.write(servoPosition);                  // tell servo to go to servoPositionition in variable 'servoPosition'
+            delay(15);                           // waits 15ms for the servo to reach the servoPositionition
+            openDone = FALSE;
+            closeDone = FALSE;
+            if(servoPosition == 0){
+                openDone = TRUE;
+            }
+        }
     }
-  }
 }
