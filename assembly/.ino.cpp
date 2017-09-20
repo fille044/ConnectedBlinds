23789dc649299528dfe1b0088b9fb25168d22bd3
Unknown <fille044@gmail.com>
2017-09-20 10:28:17 +0200

    Just commit of all files related to assembly

--- assembly/.ino.cpp
+++ assembly/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-09-19 09:44:49
+//This file has been generated on 2017-09-20 09:58:23
 
 #include "Arduino.h"
 #include <ESP8266WiFi.h>
