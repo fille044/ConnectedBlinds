f8e6fff2f56d24c48fd13076ce716fa71dedb5a0
Unknown <hannes.hedlund94@gmail.com>
2017-09-25 10:28:58 +0200

    New code with phototransistor capabilities

--- assembly2.0/assembly2.0.ino
+++ assembly2.0/assembly2.0.ino
@@ -1,18 +1,91 @@
 /* Defines */
 #include <ESP8266WiFi.h>
 #include <Servo.h>
+
 #include <WiFiClient.h>
 #include <WiFiServer.h>
 #include <WiFiUdp.h>
 
-#include "declaration.h"
+#define FALSE 0
+#define TRUE 1
+#define ON 0
+#define OFF 1
+
+#define MAX 180
+#define SERVOSPEED 20
+
+/* Static variables */
+const char* ssid = "Molk";
+const char* password = "Molk0901";
+
+int ModeState = 0;
+int blueLed = 13; 		// D7 GPIO13
+int greenLed = 12;  	// D6 GPIO12
+int button = 16;      // D0 GPIO16
+int photoTransitor = A0;
 
 int servoPosition = 0;
+int lightLevel = 0;
 bool closeDone;
 bool openDone;
 
+bool isTimeout = FALSE;
+int timeoutCounter = 0;
+
 bool isButtonPressed;
 int prevButtonState = digitalRead(16);
+
+Servo myservo;        // D5 GPIO14
+WiFiServer server(80);
+
+
+/* ------------------------------------------------------*/
+/*
+   Initialize the Wifi-communication.
+   Connects to network "Molk" and prints out the device
+   IP-adress in serial monitor.
+*/
+/* ------------------------------------------------------*/
+void initWifi(void)
+{
+  // Connect to WiFi network
+	Serial.println();
+	Serial.println();
+	delay(500);
+	Serial.print("Connecting to ");
+	Serial.println(ssid);
+
+	WiFi.begin(ssid, password);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		timeoutCounter++;
+		if (timeoutCounter > 60) {
+			isTimeout = TRUE;
+			Serial.println("Connection has timed out, entering manual mode");
+			return;
+		}
+		Serial.print(".");
+	}
+	Serial.println("");
+	Serial.println("WiFi connected");
+
+	// Start the server
+	server.begin();
+	Serial.println("Server started");
+
+	// Print the IP address
+	Serial.print("Use this URL to connect: ");
+	Serial.print("http://");
+	Serial.print(WiFi.localIP());
+	Serial.println("/");
+
+	Serial.println();
+	Serial.print("MAC: ");
+	Serial.println(WiFi.macAddress());
+	Serial.println("/");
+	Serial.println("/");
+}
 
 
 /* ------------------------------------------------------*/
@@ -33,8 +106,11 @@ void setup()
     delay(1000);
 
     pinMode(button, INPUT);
+   // pinMode(photoTransitor, INPUT);
     pinMode(blueLed, OUTPUT);
     pinMode(greenLed, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
     digitalWrite(blueLed, LOW);
     digitalWrite(greenLed, LOW);
 }
@@ -81,6 +157,35 @@ void controlServo(int Status)
 			}
 		}
 	}
+  if(Status == 2 && lightLevel > 500 && closeDone == FALSE) {
+    myservo.attach(14);
+
+    // Runs servo slowly until end is reached
+    for (int pos = MAX; pos >= 0 ; pos--) {
+      myservo.write(pos);
+      delay(SERVOSPEED);
+      if (pos == 0) {
+        closeDone = TRUE;
+        openDone = FALSE;
+        Serial.println("Close done");
+        myservo.detach(); // To eliminate noise
+      }
+    }
+  }
+  if(Status == 2 && lightLevel < 450 && openDone == FALSE) {
+    myservo.attach(14);
+    
+    for (int pos = 0; pos <= MAX ; pos++) {
+      myservo.write(pos);
+      delay(SERVOSPEED);
+      if (pos == MAX) {
+        openDone = TRUE;
+        closeDone = FALSE;
+        Serial.println("Open done");
+        myservo.detach();    // To eliminate noise
+      }
+    }
+  }
 }
 
 
@@ -95,6 +200,7 @@ void function(void)
 	enum enumStatus{
 		Open,
 		Close,
+    Photo,
 	};
 
 	switch (ModeState) {
@@ -122,7 +228,13 @@ void function(void)
         digitalWrite(greenLed, LOW);
         controlServo((int)Close);
 		break;
-	}
+ 
+  case 3:
+    //Phototransitor Auto
+        digitalWrite(LED_BUILTIN, HIGH);
+        controlServo((int)Photo);
+    break;
+  }
 }
 
 
@@ -133,8 +245,7 @@ void function(void)
 /* ------------------------------------------------------*/
 void loop()
 {
-	light = analogRead(phototransistor);
-	//Serial.println(light);
+  //lightLevel = analogRead(photoTransitor);
 	if (prevButtonState != digitalRead(16)) {
 		ModeState = 0;
 	}
@@ -142,7 +253,7 @@ void loop()
     function();
     int counter = 0;
     // Check if a client has connected
-    client = server.available();
+    WiFiClient client = server.available();
     if (!client) {
         return;
     }
@@ -174,16 +285,17 @@ void loop()
 
         ModeState = 2;
     }
+    if(request.indexOf("/Phototransistor") != -1) {
 
+        ModeState = 3;
+    }
 
     // Return the response
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
     client.println(""); //  do not forget this one
-
     client.println("<!DOCTYPE HTML>");
     client.println("<html>");
-
     client.println("<head><title>ConnectedBlinds</title></head>");
     client.println("<body>");
     client.print("Mode is now: ");
@@ -198,12 +310,13 @@ void loop()
         client.print("Automatic closed");
     }
 
-    client.println("<br>-----------------------------------------------</br><br><br>");
-    client.println("<a href=\"/Manual\"\"><button>Manual</button></a><br><br>");
+    client.println("<br>-----------------------------------------------</br>");
+    client.println("<br><br>");
+    client.println("<a href=\"/Manual\"\"><button>Manual</button></a>");
+    client.println("<a href=\"/Phototransistor\"\"><button>Phototransistor</button></a>");
+    client.println("<br><br>");
     client.println("<a href=\"/Open\"\"><button>Open</button></a>");
     client.println("<a href=\"/Closed\"\"><button>Closed</button></a>");
-	client.println("<br>-----------------------------------------------</br>");
-
     client.println("</body>");
     client.println("</html>");
 
